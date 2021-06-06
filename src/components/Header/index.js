a3e48f5e1f08a3efc7397d417b96a40a96b337d8
ivanvb <ivanvalette@hotmail.com>
2021-06-06 15:48:48 -0400

    fix: update navbar links and padding

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,18 @@ const links = [
         title: 'About',
         url: '/about',
     },
+    {
+        title: 'Commissions',
+        url: '/commisions',
+    },
 ];
 
 const Header = () => {
     return (
-        <header className="bg-primary py-8">
+        <header className="bg-primary py-4 md:py-8">
             <nav className="container flex justify-between items-center">
                 <div className="h-16 w-16 bg-gray-500"></div>
-                <ul className="text-lg">
+                <ul className="text-lg flex space-x-3">
                     {links.map((link, i) => (
                         <li key={i}>
                             <Link to={link.url}>{link.title}</Link>
