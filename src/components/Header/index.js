71a132c897504ebac56db51824d60cf6a0972c61
ivanvb <ivanvalette@hotmail.com>
2021-06-06 21:33:56 -0400

    feat: add logo and made navbar sticky

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import ThemeToggler from '../ThemeToggler/ThemeToggler';
+import Logo from '../icons/Logo';
 
 const links = [
     {
@@ -15,17 +16,22 @@ const links = [
 
 const Header = () => {
     return (
-        <header className="py-4 md:py-8 text-primary-contrast">
+        <header className="py-4 md:py-8 text-primary-contrast sticky top-0 bg-primary z-50">
             <nav className="container flex justify-between items-center">
-                <Link className="h-16 w-16" to="/">
-                    <div className="h-full w-full bg-gray-500"></div>
+                <Link className="h-16 w-28 md:w-40" to="/">
+                    <Logo className="h-full w-full" />
                 </Link>
-                <div className="flex space-x-8">
+                <div className="flex space-x-4 md:space-x-8">
                     <ThemeToggler />
-                    <ul className="text-lg flex space-x-3">
+                    <ul className="text-base md:text-lg flex space-x-3 tracking-wide">
                         {links.map((link, i) => (
                             <li key={i}>
-                                <Link to={link.url}>{link.title}</Link>
+                                <Link
+                                    to={link.url}
+                                    activeClassName="border-b-2 border-primary-accent pb-1"
+                                >
+                                    {link.title}
+                                </Link>
                             </li>
                         ))}
                     </ul>
