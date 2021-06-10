3fd85c72e5a3ccb4676dc7b5c43a73841f8a692d
ivanvb <ivanvalette@hotmail.com>
2021-06-09 21:00:37 -0400

    feat: add aboutpage data from cms

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,18 +4,16 @@ import CommisionsBanner from '../components/CommissionsBanner/CommisionsBanner';
 import Seo from '../components/Seo';
 import { graphql } from 'gatsby';
 
-const About = ({ data }) => {
+const About = ({ data: { datoCmsAboutPage: pageData } }) => {
     return (
         <>
             <Seo title="About" />
             <PageIntro
-                img={data.file.childImageSharp.gatsbyImageData}
-                title="About the Author"
-                subtitle="Who is the person behind these paintings?"
-                paragraphs={[
-                    'Dolore enim esse voluptate reprehenderit eiusmod sit irure pariatur adipisicing aliquip sint sunt laborum ea. Ipsum ipsum nulla elit cillum laborum laborum ea ad eiusmod aute ut. Ad sunt sit reprehenderit irure labore tempor adipisicing magna labore aute dolor amet ut. Voluptate fugiat incididunt aliquip Lorem labore ea occaecat minim ut sunt. Non non id labore voluptate consectetur consectetur veniam laboris ut officia id duis. Consectetur qui occaecat ex anim. Mollit eiusmod sint est in incididunt nulla.',
-                    'Laboris sint eu aute labore laborum magna. Nostrud sint ipsum Lorem est excepteur velit. Amet reprehenderit do reprehenderit pariatur do tempor amet dolore adipisicing non. Et exercitation laborum elit voluptate 😎. Eiusmod minim nulla labore eu enim ipsum dolor fugiat ipsum incididunt non. Fugiat voluptate consectetur culpa et.',
-                ]}
+                img={pageData.image.gatsbyImageData}
+                alt={pageData.image.alt}
+                title={pageData.title}
+                subtitle={pageData.subtitle}
+                paragraphs={pageData.description.split('\n\n')}
                 reverse
             />
             <CommisionsBanner />
@@ -25,9 +23,13 @@ const About = ({ data }) => {
 
 export const query = graphql`
     query {
-        file(name: { eq: "dartwork-1" }) {
-            childImageSharp {
-                gatsbyImageData(width: 1140)
+        datoCmsAboutPage {
+            title
+            subtitle
+            description
+            image {
+                gatsbyImageData(width: 1160)
+                alt
             }
         }
     }
